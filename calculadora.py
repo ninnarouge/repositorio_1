9ae863429d8c06bef788240caa4c709e84916f34
sanuel_aa <sa.parrac@duocuc.cl>
2024-06-14 20:22:28 -0400

    input x opcion
    pide input por cada opcion

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,4 @@
-num1 = int(input("numero 1: ")) 
-num2 = int(input("numero 2: "))
-num3 = int(input("numero 3: "))  #Agregado
+
 
 valor = 0
 while True:
@@ -8,27 +6,40 @@ while True:
             1- Sumar 
             2- Restar
             3- Multiplicar
-            4- dividir
+            4- dividir             
             5- sumar 3 valores 
+
         """)
-# linea 12 Agregado
+
     valor = int(input("Elige una opcion: ") )     
 
     if valor == 1:
+        num1 = int(input("numero 1: ")) 
+        num2 = int(input("numero 2: ")) 
         print("la suma es",num1+num2)
         break;
     if valor == 2:
+        num1 = int(input("numero 1: ")) 
+        num2 = int(input("numero 2: ")) 
         print("la resta es",num1-num2)
         break;
     if valor == 3:
+        num1 = int(input("numero 1: ")) 
+        num2 = int(input("numero 2: ")) 
         print("la multiplicacion es",num1*num2)
         break;
     if valor == 4:
+        num1 = int(input("numero 1: ")) 
+        num2 = int(input("numero 2: ")) 
         print("la division es",num1/num2)
         break;
-    if valor == 5: #opción agregada
-        print('La suma de los valores es', num1+num2+num3)
+    if valor == 5: #update
+        num1 = int(input("numero 1: ")) 
+        num2 = int(input("numero 2: ")) 
+        num3 = int(input("numero 3: "))
+        print("la suma es",num1+num2+num3)
         break;
+
     else:
         print("Opcion incorrecta")
         break;
